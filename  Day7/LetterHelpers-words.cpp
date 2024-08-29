166e17bdf6ad11873e9d58670c815f8620b6c126
hijiia <hijiia@icloud.com>
2024-08-28 18:20:14 -0600

    add heelo to my repo

---  Day7/LetterHelpers-words.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  LetterHelpers-words.cpp
-//   Day7
-//
-//  Created by Jia Gao on 8/27/24.
-//
-
-#include "LetterHelpers-words.hpp"
--- HELLO WORLD/HELLO WORLD/main.cpp
+++ HELLO WORLD/HELLO WORLD/main.cpp
@@ -1,14 +1,12 @@
 //
 //  main.cpp
-//  Poker
+//  HELLO WORLD
 //
 //  Created by Jia Gao on 8/28/24.
 //
-
 #include <iostream>
-
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
+    std::cout << "hello world" << std::endl ;
 }
+
