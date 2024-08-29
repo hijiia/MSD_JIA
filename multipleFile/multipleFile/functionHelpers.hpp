bb664e0937b9d6dd08e4572fe1793be9e8593009
hijiia <hijiia@icloud.com>
2024-08-28 19:31:41 -0600

    add multiplefile to my repo

---
+++ multipleFile/multipleFile/functionHelpers.hpp
@@ -0,0 +1,30 @@
+//
+//  functionHelpers.hpp
+//  multipleFile
+//
+//  Created by Jia Gao on 8/28/24.
+//
+
+#ifndef functionHelpers_hpp
+#define functionHelpers_hpp
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+#endif /* LetterHelpers_hpp */
+bool IsVowel (char c);
+bool IsConsonant (char c);
+int NumVowels (std::string s);
+int NumConsonants(std::string s);
+bool IsTerminator (char c);
+bool IsPunctuation(std::string s);
+int NumWords(std::string s);
+int NumSentences(std::string s);
+double AverageWordLength(std::string s);
+double AverageVowelsPerWord(std::string s);
+
+
+
+
+
---
+++ multipleFile/multipleFile/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  multipleFile
+//
+//  Created by Jia Gao on 8/28/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
